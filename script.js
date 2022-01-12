fe363e0118ad13cfa6410c7560724e49fff8ee84
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-12 19:19:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -758,6 +758,7 @@ console.log(day_of_the_month(d));
 
 
 
+
 // Write a JavaScript function to get the day of the month, 2 digits with leading zeros.   
 // Test Data :
 // d= new Date(2015, 10, 1);
